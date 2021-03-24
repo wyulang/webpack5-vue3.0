51d59f28c8d0b9bb1b3a20899ab4373afa8240e0
wyulang <37110078+wyulang@users.noreply.github.com>
2021-03-24 13:01:56 +0800

    Update server.js

--- webpack/server.js
+++ webpack/server.js
@@ -6,7 +6,17 @@ let config = require('./webpack.config.js');
 
 const compiler = webpack(webpackConfig);
 const devServerOptions = Object.assign({}, webpackConfig.devServer);
-const server = new WebpackDevServer(compiler, devServerOptions);
 
+// Object.assign(devServerOptions, {
+//   proxy: {
+//     '/api': {
+//       target: 'http://demo.ndky.edu.cn',  // 正式环境
+//       changeOrigin: true,
+//       pathRewrite: { '^/api': '' }
+//     }
+//   }
+// })
+
+const server = new WebpackDevServer(compiler, devServerOptions);
 
 server.listen(config.port, config.host, res => {});
