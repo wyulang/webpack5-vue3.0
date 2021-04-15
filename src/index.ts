f3d57038db7983eeb57ea9fa3654aee5216b4fc1
wyulang <37110078+wyulang@users.noreply.github.com>
2021-04-15 15:54:57 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
   
-/*
- * @description: 开发模式，vue需要完整入口文件
- * @Author: Gouxinyu
- * @Date: 2020-11-09 00:56:37
- */
 import { createApp } from "vue";
 import box from "./app.vue";
 import { router } from './router';
